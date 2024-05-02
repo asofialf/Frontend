84b29fbde461c91778d22238dd1c1ec927386211
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2024-05-01 20:58:59 -0500

    feat: basic home "Mi flota" layout

--- src/app/modules/planification/pages/bus-fleet/views/bus-fleet-home-page/bus-fleet-home-page.component.ts
+++ src/app/modules/planification/pages/bus-fleet/views/bus-fleet-home-page/bus-fleet-home-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatCard, MatCardContent} from "@angular/material/card";
 
 @Component({
   selector: 'app-bus-fleet-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardContent
+  ],
   templateUrl: './bus-fleet-home-page.component.html',
   styleUrl: './bus-fleet-home-page.component.scss'
 })
