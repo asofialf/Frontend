198f12227073357570ab9b36446b9cb0555c3ecc
Tania Vásquez <U20181B530@UPC.EDU.PE>
2024-05-03 09:56:04 -0500

    fix: Traductions

--- src/app/context/shared/layout/main-layout/main-layout.component.ts
+++ src/app/context/shared/layout/main-layout/main-layout.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import { MatIcon } from '@angular/material/icon';
 import {HttpClientModule} from "@angular/common/http";
+
+import {UserProfileCardComponent} from "../../components/user-profile-card/user-profile-card.component";
+import {AccountService} from "../../../account/application/service/account.service";
+
+import {UserProfileCardInformation} from "../../../account/domain/models/userProfileCardInformation";
+
 
 @Component({
   selector: 'app-main-layout',
@@ -16,11 +22,18 @@ import {HttpClientModule} from "@angular/common/http";
     MatListModule,
     MatIcon,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    UserProfileCardComponent
   ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
 export default class MainLayoutComponent {
+
+  currentUser:UserProfileCardInformation;
+
+  constructor(private router:Router,private accountService:AccountService) {
+    this.currentUser= this.accountService.getCurrentUser()
+  }
 
 }
