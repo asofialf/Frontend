0319cc79c7bb7a2fb9af6775c608b010c318ddb1
DiegoMerinoMas <u202111473@upc.edu.pe>
2024-05-29 21:39:05 -0500

    feat: Dialog almost complete, verification in process

--- src/app/context/planification/interface/pages/departure-schedule/create-new-departure-schedule/components/departure-time-dialog/departure-time-dialog.component.ts
+++ src/app/context/planification/interface/pages/departure-schedule/create-new-departure-schedule/components/departure-time-dialog/departure-time-dialog.component.ts
@@ -9,6 +9,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-departure-time-dialog',
@@ -23,11 +24,18 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatInputModule,
     MatIconModule,
     MatDialogModule,
+    MatSelectModule,
   ],
   templateUrl: './departure-time-dialog.component.html',
   styleUrls: ['./departure-time-dialog.component.scss'],
 })
 export class DepartureTimeDialogComponent implements OnInit {
+  hourOptions: number[] = Array.from({ length: 24 }, (_, i) => i);
+  minuteOptions: number[] = Array.from({ length: 60 }, (_, i) => i);
+  hours: number[] = [];
+  minutes: number[] = [];
+  is24HourFormat: boolean = true;
+
   departureTimes: string[] = [];
   time: string = '';
 
@@ -41,13 +49,90 @@ export class DepartureTimeDialogComponent implements OnInit {
   }
 
   initializeDepartureTimes() {
+    const now = new Date();
+    const selectedDate = new Date(this.data.date);
+
+    // Verificar si la fecha seleccionada es diferente a la fecha actual
+    if (
+      selectedDate.getFullYear() === now.getFullYear() &&
+      selectedDate.getMonth() === now.getMonth() &&
+      selectedDate.getDate() === now.getDate()
+    ) {
+      // La fecha seleccionada es la misma que la fecha actual, devolver un error o mostrar un mensaje
+      console.error(
+        'La fecha seleccionada no puede ser la misma que la fecha actual.'
+      );
+      return;
+    }
+
     for (let i = 0; i < this.data.numberOfLaps; i++) {
       this.departureTimes.push('');
+      this.hours.push(selectedDate.getHours());
+      this.minutes.push(selectedDate.getMinutes());
     }
   }
 
+  toggleTimeFormat() {
+    this.is24HourFormat = !this.is24HourFormat;
+  }
+
   hasEmptyDepartureTimes(): boolean {
-    return this.departureTimes.some((time) => !time);
+    let previousDepartureTime: Date | null = null;
+
+    for (let i = 0; i < this.departureTimes.length; i++) {
+      const departureTime = this.getDepartureTime(i);
+
+      if (!departureTime) {
+        return true; // Empty departure time
+      }
+
+      if (previousDepartureTime && departureTime <= previousDepartureTime) {
+        return true; // Departure time is not after the previous one
+      }
+
+      previousDepartureTime = departureTime;
+    }
+
+    return false;
+  }
+
+  getDepartureTime(index: number): Date | null {
+    const hour = this.hours[index];
+    const minute = this.minutes[index];
+    const now = new Date();
+
+    if (hour === null || minute === null) {
+      return null;
+    }
+
+    const amPm = this.is24HourFormat ? undefined : hour >= 12 ? 'PM' : 'AM';
+    const formattedHour = this.is24HourFormat ? hour : hour % 12 || 12;
+    const formattedMinute = minute.toString().padStart(2, '0');
+    const timeString = `${formattedHour}:${formattedMinute} ${amPm}`;
+    const departureTime = new Date(`1970-01-01T${timeString}`);
+    // Verificar si la hora seleccionada es anterior a la hora actual
+    if (departureTime < now) {
+      // La hora seleccionada es anterior a la hora actual, devolver un error o mostrar un mensaje
+      console.error(
+        'La hora seleccionada no puede ser anterior a la hora actual.'
+      );
+      return null;
+    }
+    return new Date(`1970-01-01T${timeString}`);
+  }
+
+  formatHour(hour: number): string {
+    if (this.is24HourFormat) {
+      return hour.toString().padStart(2, '0');
+    } else {
+      const formattedHour = hour % 12 || 12;
+      const amPm = hour >= 12 ? 'PM' : 'AM';
+      return `${formattedHour} ${amPm}`;
+    }
+  }
+
+  formatMinute(minute: number): string {
+    return minute.toString().padStart(2, '0');
   }
 
   onCancel() {
@@ -55,7 +140,13 @@ export class DepartureTimeDialogComponent implements OnInit {
   }
 
   onAddToTable() {
-    // Logic to add the departure schedule to the table
+    const departureTimes = this.hours.map((hour, index) => {
+      const minute = this.minutes[index];
+      const amPm = this.is24HourFormat ? undefined : hour >= 12 ? 'PM' : 'AM';
+      const formattedHour = this.is24HourFormat ? hour : hour % 12 || 12;
+      const formattedMinute = minute.toString().padStart(2, '0');
+      return `${formattedHour}:${formattedMinute} ${amPm}`;
+    });
     this.dialogRef.close({ departureTimes: this.departureTimes });
   }
 }
