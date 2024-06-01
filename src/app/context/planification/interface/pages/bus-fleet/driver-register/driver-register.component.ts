4ba4a265b04215311a3ce317f7f87a8e269e7442
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2024-05-31 23:41:12 -0500

    fix: update driver register view

--- src/app/context/planification/interface/pages/bus-fleet/driver-register/driver-register.component.ts
+++ src/app/context/planification/interface/pages/bus-fleet/driver-register/driver-register.component.ts
@@ -1,89 +1,98 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component } from '@angular/core';
+import {DriverCardComponent} from "../../../components/bus-fleet/driver-card/driver-card.component";
 import {MatButton} from "@angular/material/button";
-import {MatTableModule} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {Location, NgForOf, NgIf} from "@angular/common";
 import {UserProfileCardComponent} from "../../../../../shared/components/user-profile-card/user-profile-card.component";
 import {UserProfileCardInformation} from "../../../../../account/domain/models/userProfileCardInformation";
 import {AccountService} from "../../../../../account/application/service/account.service";
-import {Router, RouterOutlet} from "@angular/router";
 import {DriversService} from "../../../../application/service/drivers.service";
-import {Driver} from "../../../../domain/models/driver";
-import {HttpClientModule} from "@angular/common/http";
-import {NgForOf, NgIf, Location} from "@angular/common";
-import {DriverCardComponent} from "../../../components/bus-fleet/driver-card/driver-card.component";
-import {MatPaginatorModule, PageEvent} from "@angular/material/paginator";
-import {MatDividerModule} from '@angular/material/divider';
-import { MatIcon } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
+import { BackButtonComponent } from '../../../../../shared/components/back-button/back-button.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @Component({
-  selector: 'app-driver-administration',
+  selector: 'app-driver-register',
   standalone: true,
   imports: [
-    MatTableModule,
-    RouterOutlet,
-    MatButton,
-    HttpClientModule,
-    UserProfileCardComponent,
-    NgForOf,
     DriverCardComponent,
-    MatPaginatorModule,
+    MatButton,
+    MatPaginator,
+    NgForOf,
     NgIf,
-    MatDividerModule,
-    MatIcon,
-    MatButtonModule
+    UserProfileCardComponent,
+    ReactiveFormsModule,
+    BackButtonComponent,
+    MatFormFieldModule,
+    MatInputModule
   ],
-  templateUrl: '../driver-administration/driver-administration.component.html',
-  styleUrl: '../driver-administration/driver-administration.component.scss'
+  templateUrl: './driver-register.component.html',
+  styleUrl: './driver-register.component.scss'
 })
-export class DriverAdministrationComponent implements OnInit{
-
-  defaultImage=''
-
-  drivers: Driver[] = [];
-  paginatedDrivers:Driver[]=[];
-  pageSize=6;
-
-  currentUser:UserProfileCardInformation;
+export class DriverRegisterComponent {
+  currentUser: UserProfileCardInformation;
+  registerForm: FormGroup;
+  isSubmitted=false;
 
   constructor(
-    private router:Router,
-    private accountService:AccountService,
-    private driversService:DriversService,
-    private location: Location
-  ) {
-    this.currentUser= this.accountService.getCurrentUser()
-  }
-
-  ngOnInit(){
-    this.loadDrivers();
-  }
-
-  loadDrivers(): void {
-    this.driversService.getAllDrivers().subscribe({
-      next: (data) => {
-        this.drivers = data;
-        if (data.length > 0) {
-          this.updatePaginatedDrivers(0);
-        }
-      },
-      error: (err) => console.error('Error fetching drivers:', err)
+    private location:Location,
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private driversService:DriversService
+  )
+  {
+    this.currentUser = this.accountService.getCurrentUser();
+    this.registerForm = this.fb.group({
+      photo: [''],
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      dni: ['', Validators.required],
+      phone_number: ['', Validators.required, Validators.pattern('^[0-9]*$')],
+      email: ['', Validators.required, Validators.email],
+      license_number: ['', Validators.required],
+      license_photo: ['']
     });
   }
 
-  handlePageEvent(event: PageEvent):void{
-    this.updatePaginatedDrivers(event.pageIndex*event.pageSize);
-  }
-
-  updatePaginatedDrivers(startIndex: number): void {
-    this.paginatedDrivers = this.drivers.slice(startIndex, startIndex + this.pageSize);
-  }
-
-  navigateToAddNewDriver(): void {
-    this.router.navigate(['/bus-fleet/drivers/add-new-driver']);
-  }
-
-  goBack(): void {
+  goBack(){
     this.location.back();
   }
+
+  onSubmit() {
+    console.log('Submitted form', this.registerForm.value);
+    if (!this.registerForm.invalid) {
+      this.driversService.addDriver(this.registerForm.value).subscribe({
+        next: (response) => {
+          console.log('Driver added', response);
+          this.goBack(); // Opcional: regresar tras añadir
+        },
+        error: (error) => {
+          console.error('Error adding driver', error);
+        }
+      });
+    }
+    this.isSubmitted = true;
+  }
+
+  shouldShowError(controlName: string): boolean {
+    const control = this.registerForm.get(controlName);
+    return <boolean>control?.invalid && (control?.touched || this.isSubmitted);
+  }
+
+  resetForm() {
+    this.registerForm.reset({
+      photo: '',
+      first_name: '',
+      last_name: '',
+      dni: '',
+      phone_number: '',
+      email: '',
+      license_number: '',
+      license_photo: ''
+    });
+    this.isSubmitted = false; // Resetear también el indicador de envío
+  }
+
 
 }
