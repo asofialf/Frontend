85ab410d4b6e0f5df55f3d8c5beeb43f5196d605
Tania Vásquez <U20181B530@UPC.EDU.PE>
2024-06-08 12:06:44 -0500

    fix: route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,29 @@
 import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
+
   {
-    path: 'sign-in',
-    title: 'Sign In',
-    loadComponent: () => import('./context/public/pages/sign-in/sign-in.component').then(m => m.SignInComponent),
-  },
+    path: '',
+    loadComponent: () => import('./context/shared/pages/main-layout/main-layout.component').then(m => m.default),
+    children: [
+      {
+        path: 'sign-in',
+        title: 'Sign In',
+        loadComponent: () => import('./context/public/pages/sign-in/sign-in.component').then(m => m.SignInComponent),
+      },
+      {
+        path: 'sign-up',
+        title: 'Sign Up',
+        loadComponent: () => import('./context/public/pages/sign-up/sign-up.component').then(m => m.SignUpComponent),
+      },
+      {
+        path: 'create-transport-company',
+        loadComponent: () => import('./context/planification/pages/transport-company/transport-company.component').then(m => m.TransportCompanyComponent),
+      },
+    ]
+  }, 
   {
-    path: 'sign-up',
-    title: 'Sign Up',
-    loadComponent: () => import('./context/public/pages/sign-up/sign-up.component').then(m => m.SignUpComponent),
-  },
-  {
-    path: 'create-transport-company',
-    loadComponent: () => import('./context/planification/pages/transport-company/transport-company.component').then(m => m.TransportCompanyComponent),
+    path: '**',
+    redirectTo: '/sign-in'
   }
 ];
